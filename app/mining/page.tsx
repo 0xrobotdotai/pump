4663cbbc3b9fa9d168bfa1c98de9ac4523248850
Jevin_F <jevinfsmail@gmail.com>
2024-12-14 10:22:19 +0800

    fix mining text

--- app/mining/page.tsx
+++ app/mining/page.tsx
@@ -9,9 +9,9 @@ const Mining: React.FC = () => {
         <Image src="/imgs/SSR.jpg" className="w-[300px] h-[300px] lg:w-[500px] md:h-[500px] flex-none" alt=""></Image>
       </div>
       <div className="">
-        <div className="text-3xl font-semibold mt-20 text-primary">AI Warrior Mining Coming Soon</div>
-        <div className="my-6 md:text-xl flex-1 mb-10">Holders of the AI Warrior NFT will have the opportunity to mine 1% of the total tokens successfully released to Mimo. Stay tuned for updates!</div>
-        <Button variant="bordered" className="w-[200px]" color="primary" as={Link} href="https://www.0xrobot.ai/nft">Buy NFT</Button>
+        <div className="text-3xl font-semibold mt-20 text-primary">Robot AI Warrior NFT Mining Rewards</div>
+        <div className="my-6 md:text-xl flex-1 mb-10">Holders of the AI Warrior NFT have the unique opportunity to mine the tokens successfully launched from the RobotPump platform.</div>
+        <Button variant="bordered" className="w-[200px]" color="primary" as={Link} href="https://www.0xrobot.ai/nft">Join Now !</Button>
       </div>
     </main>
   );
