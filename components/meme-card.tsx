2d8364f04d46994e742ea9d28bd813f071734358
Jevin_F <jevinfsmail@gmail.com>
2024-12-19 10:40:32 +0800

    fix meme card image

--- components/meme-card.tsx
+++ components/meme-card.tsx
@@ -60,7 +60,7 @@ const MemeCard = observer(() => {
       ) : (
         <Link href={`/token/${token?.id}`} className="flex h-[120px] lg:h-[160px] justify-start items-start cursor-pointer overflow-hidden rounded-xl border-2 border-primary/20 bg-gradient-to-r  to-[#92E7FF] from-[#FED25B] via-[#F3AEF5] hover:border-primary" key={token?.id}>
           <div className="flex-none h-[120px] lg:h-[160px] w-[120px] lg:w-[160px] bg-[#151527]/30">
-            <ImageFall src={token?.image || ''} alt={token?.name} className="w-full h-full object-cover" />
+            <ImageFall src={token?.image || ''} alt={token?.name} className="w-full h-full object-contain" />
           </div>
           <div className="flex-1 h-full flex-col gap-1 lg:gap-3 justify-start items-start flex p-2 lg:p-4 bg-[#151527] text-white/50">
             <div className="text-center text-xs leading-none opacity ">
