90bd6b2fdd74e94303496e63af2b85136b8c11bc
Jevin_F <jevinfsmail@gmail.com>
2025-07-11 16:59:59 +0800

    fix ban tokens

--- store/market.ts
+++ store/market.ts
@@ -36,6 +36,22 @@ export class MarketStore {
   itemsPerPage: number = 12;
   currentPage: number = 1;
 
+  // 被禁用的token列表
+  banTokens: string[] = [
+    "0x3bd141cc88de5acbe8dc8ea3d90674bddd0c72c6",
+    "0xa57abb6ad3681ec901084507dad8d5afc311af4a",
+    "0x3199cf69067e50742117465cf3fb511a81abec2d",
+    "0x01d6fc6c64e0b13dc3ba4709debdd0f29f54b9f5",
+    "0xca5a4e34289325efe2dc34ae263029430bc92439",
+    "0xc1fc5269a39acd45a832142fdd18311a6df2f603",
+    "0x78a6b4663b70e19a49199e352c6b351b4c111133",
+    "0x3b478620dfacfd097afc429259b3e11b2976bd60",
+    "0x277c833d72af8129b67903645852b891eb5f49c3",
+    "0x543d3c8138213869725022e827781412f9851d63",
+    "0xb203b3ca70d2f8db8fd73a7b5915d3eee2e43646",
+    "0x9b0eeccfb0093ae60da6b67fe6e9bb1114458cae",
+  ];
+
   listType: ListType = "card";
   listTypes: { type: ListType; icon: React.FC }[] = [
     {
@@ -98,7 +114,7 @@ export class MarketStore {
 
   changeList(v: boolean) {
     this.listed = v;
-    this.currentPage = 1
+    this.currentPage = 1;
     this.getTokens.execute();
   }
 
@@ -156,8 +172,10 @@ export class MarketStore {
   });
 
   getRobotPads = new AsyncState({
-    action:async () => {
-      const data: { robotPads: { totalToken: string; totalLaunched: string }[] } = await client.request(
+    action: async () => {
+      const data: {
+        robotPads: { totalToken: string; totalLaunched: string }[];
+      } = await client.request(
         gql`
           query {
             robotPads {
@@ -173,7 +191,7 @@ export class MarketStore {
   });
 
   getTokens = new AsyncState({
-    action:async () => {
+    action: async () => {
       try {
         this.loading = true;
         let variables: any = {
@@ -181,15 +199,23 @@ export class MarketStore {
           skip: (this.currentPage - 1) * this.itemsPerPage,
           orderBy: this.sortBy,
           orderDirection: this.sortOrder,
+          banTokens: this.banTokens,
         };
 
         const isEmpyt = this.keyword == "";
 
         const data: { tokens: TOKEN[] } = await client.request(
           gql`
-            query ($first: Int!, $skip: Int!, $orderBy: String, $orderDirection: String) {
+            query ($first: Int!, $skip: Int!, $orderBy: String, $orderDirection: String, $banTokens: [String!]!) {
               tokens(first: $first, skip: $skip, orderBy: $orderBy, orderDirection: $orderDirection, blocked: false, 
-              where:  {${isEmpyt ? "" : `or: [{name_starts_with_nocase: "${this.keyword}"}, {symbol_starts_with_nocase: "${this.keyword}"}],`} ${this.listed ? `launchedAt_gte: "0"` : ""}
+              where:  {
+                id_not_in: $banTokens
+                ${
+                  isEmpyt
+                    ? ""
+                    : `or: [{name_starts_with_nocase: "${this.keyword}"}, {symbol_starts_with_nocase: "${this.keyword}"}],`
+                } 
+                ${this.listed ? `launchedAt_gte: "0"` : ""}
               }
             ) 
             {
@@ -209,12 +235,13 @@ export class MarketStore {
           `,
           variables
         );
+        console.log("data", data);
 
         this.tokens = data?.tokens?.map((token: TOKEN) => {
-          const data = this.fomatTokenInfo(token.info)
+          const data = this.fomatTokenInfo(token.info);
           return {
             ...token,
-            ...data
+            ...data,
           };
         });
 
@@ -228,13 +255,19 @@ export class MarketStore {
   });
 
   getRandomToken = new AsyncState({
-    action:async () => {
+    action: async () => {
       try {
         this.loading = true;
         const data: { tokens: TOKEN[] } = await client.request(
           gql`
-            query ($first: Int!) {
-              tokens(first: $first, orderBy: createdAt, orderDirection: desc, blocked: false, where: {completed: false}) {
+            query ($first: Int!, $banTokens: [String!]!) {
+              tokens(
+                first: $first
+                orderBy: createdAt
+                orderDirection: desc
+                blocked: false
+                where: { completed: false, id_not_in: $banTokens }
+              ) {
                 id
                 name
                 symbol
@@ -251,14 +284,15 @@ export class MarketStore {
           `,
           {
             first: 10,
+            banTokens: this.banTokens,
           }
         );
 
         const filterTokens = data?.tokens?.map((token: TOKEN) => {
-          const data = this.fomatTokenInfo(token.info)
+          const data = this.fomatTokenInfo(token.info);
           return {
             ...token,
-            ...data
+            ...data,
           };
         });
         return filterTokens[Math.floor(Math.random() * filterTokens.length)];
@@ -272,13 +306,18 @@ export class MarketStore {
 
   getMyToken = new AsyncState({
     value: [] as TOKEN[],
-    action:async (address: string) => {
+    action: async (address: string) => {
       try {
         this.loading = true;
         const data: { tokens: TOKEN[] } = await client.request(
           gql`
             query ($creator: String!) {
-              tokens(where: { creator: $creator }, orderBy: createdAt, orderDirection: desc, blocked: false) {
+              tokens(
+                where: { creator: $creator }
+                orderBy: createdAt
+                orderDirection: desc
+                blocked: false
+              ) {
                 id
                 name
                 symbol
@@ -301,10 +340,10 @@ export class MarketStore {
 
         return (
           data?.tokens?.map((token: TOKEN) => {
-            const data = this.fomatTokenInfo(token.info)
+            const data = this.fomatTokenInfo(token.info);
             return {
               ...token,
-              ...data
+              ...data,
             };
           }) || []
         );
@@ -319,36 +358,38 @@ export class MarketStore {
   extractFilenameFromUrl(url: string) {
     const hashMatch = url.match(/ipfs\/([^?]+)/);
     const filenameMatch = url.match(/filename=([^&]+)/);
-  
+
     if (hashMatch && hashMatch[1] && filenameMatch && filenameMatch[1]) {
       const hash = hashMatch[1];
-      const extension = filenameMatch[1].split('.').pop();
-      return `${hash}.${extension === 'jpg' ? 'jpeg' : extension}`;
+      const extension = filenameMatch[1].split(".").pop();
+      return `${hash}.${extension === "jpg" ? "jpeg" : extension}`;
     } else {
       throw new Error("Invalid URL: Missing IPFS hash or filename");
     }
   }
-  
 
-
-  fomatTokenInfo (tokenInfo: string) {
+  fomatTokenInfo(tokenInfo: string) {
     const info = JSON.parse(tokenInfo);
-    let url = info?.image
+    let url = info?.image;
     const isWrongCdn = info.image?.includes("cdn.img2ipfs.com");
-    if(isWrongCdn) {
-      const urlName = this.extractFilenameFromUrl(info.image)
-      url = isWrongCdn ? `https://robotai-pump.mypinata.cloud/ipfs/QmaaMgXhTHFd4EA3R2DoVGrA4N7pT1VfjJtLcLY7z9BTUm/${urlName}` : info.image;
-    } 
-    
-    const isWrongipfs = info.image?.includes("i2.img2ipfs.com");
-    if(isWrongipfs) {
-      const urlName = this.extractFilenameFromUrl(info.image)
-      url = isWrongCdn ? `https://robotai-pump.mypinata.cloud/ipfs/QmaaMgXhTHFd4EA3R2DoVGrA4N7pT1VfjJtLcLY7z9BTUm/${urlName}` : info.image;
+    if (isWrongCdn) {
+      const urlName = this.extractFilenameFromUrl(info.image);
+      url = isWrongCdn
+        ? `https://robotai-pump.mypinata.cloud/ipfs/QmaaMgXhTHFd4EA3R2DoVGrA4N7pT1VfjJtLcLY7z9BTUm/${urlName}`
+        : info.image;
     }
-    
+
+    const isWrongipfs = info.image?.includes("i2.img2ipfs.com");
+    if (isWrongipfs) {
+      const urlName = this.extractFilenameFromUrl(info.image);
+      url = isWrongCdn
+        ? `https://robotai-pump.mypinata.cloud/ipfs/QmaaMgXhTHFd4EA3R2DoVGrA4N7pT1VfjJtLcLY7z9BTUm/${urlName}`
+        : info.image;
+    }
+
     return {
       ...info,
-      image: url
-    }
+      image: url,
+    };
   }
 }
